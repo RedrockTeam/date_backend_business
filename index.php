3f00da955ef828845973690e527f66b83feaa7d0
Lich2013 <longzy2013@gmail.com>
2015-08-06 15:42:55 +0800

    Access-Control-Allow-Origin: *

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 // +----------------------------------------------------------------------
 
 // 应用入口文件
-
+header("charset=utf-8");
+header("Access-Control-Allow-Origin: *");//TODO temp
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
