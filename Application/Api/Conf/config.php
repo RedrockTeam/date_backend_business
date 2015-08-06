cbfb6b701cf79be21b1176fba032075a2f69b6b6
Lich2013 <longzy2013@gmail.com>
2015-08-06 16:23:13 +0800

    route config

---
+++ Application/Api/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DEFAULT_MODULE'       =>    'Api',  // 默认模块
+    /**
+     * 路由配置
+     */
+    'URL_ROUTER_ON'   => true,
+    'URL_ROUTE_RULES' => [
+        //公共
+        'index2/index' => 'Index/index',//广告位
+    ],
+);
