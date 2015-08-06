8c9268b36d6b5e3856e0ad6f70e2db0e3e69178c
Lich2013 <longzy2013@gmail.com>
2015-08-06 16:21:07 +0800

    route ok

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -18,12 +18,12 @@ return array(
      * 数据库配置
     */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'date', // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
+    'DB_HOST'               =>  '', // 服务器地址
+    'DB_NAME'               =>  '', // 数据库名
+    'DB_USER'               =>  '',      // 用户名
     'DB_PWD'                =>  '',    // 密码
     'DB_PORT'               =>  '3306',     // 端口
-    //'DB_PREFIX'             =>  'hy_',    	// 数据库表前缀
+    'DB_PREFIX'             =>  '',    	// 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
     'DB_FIELDS_CACHE'       =>  false,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8mb4',
