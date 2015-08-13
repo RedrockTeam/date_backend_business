b5e26b2d96bb197564ca27b1cf2c55b9a45a0fc5
Lich2013 <longzy2013@gmail.com>
2015-08-13 22:45:29 +0800

    prepare discover

---
+++ Application/Api/Controller/DiscoverController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Api\Controller;
+use Think\Controller;
+
+class DiscoverController extends BaseController {
+
+    public function createDiscovery() {
+        $input = I('post.');
+
+    }
+}
