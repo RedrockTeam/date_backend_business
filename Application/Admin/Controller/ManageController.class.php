34ee05d90d88aa0f01b5c89bb58a6f0dde5a6d86
Lich2013 <longzy2013@gmail.com>
2015-09-08 15:53:23 +0800

    hover mouse show title

--- Application/Admin/Controller/ManageController.class.php
+++ Application/Admin/Controller/ManageController.class.php
@@ -3,14 +3,15 @@ namespace Admin\Controller;
 use Think\Controller;
 class ManageController extends BaseController {
     public function index() {
-        $data = M('admin')->where(['id' => session('admin_id')])->find();
+        $data = M('admin')->where(['admin.id' => session('admin_id')])->find();
         $this->assign('data', $data);
         $this->display();
     }
 
     public function judge() {
-        $data = M('discover')->where(['status' => 2])
+        $data = M('discover')->where(['discover.status' => 2])
                              ->join('JOIN users ON discover.user_id = users.id')
+                             ->field(['discover.id as discover_id', 'users.id as uid', 'users.nickname', 'discover.title', 'discover.caption', 'discover.content', 'discover.picture'])
                              ->select();
         $this->assign('data', $data);
         $this->display();
