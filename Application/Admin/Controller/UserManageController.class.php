efe80614c53d3e97bdaaed5506617f4833f1c635
Lich2013 <longzy2013@gmail.com>
2015-09-20 16:50:49 +0800

    some view

---
+++ Application/Admin/Controller/UserManageController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class UserManageController extends BaseController {
+
+    public function index() {
+//        M('users')->page()
+    }
+}
