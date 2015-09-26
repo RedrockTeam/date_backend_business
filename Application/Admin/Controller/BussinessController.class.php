777b59bc0d5915905550f26d6d63b46c39b7919d
Lich2013 <longzy2013@gmail.com>
2015-09-26 23:21:05 +0800

    maybe ok

--- Application/Admin/Controller/BussinessController.class.php
+++ Application/Admin/Controller/BussinessController.class.php
@@ -11,4 +11,57 @@ class BussinessController extends BaseController {
         $this->assign('data', $data);
         $this->display();
     }
+
+    //通过
+    public function pass() {
+        $id = I('post.id');
+        M('users')->where(['id' => $id])->save(['status' => 1]);
+        $this->ajaxReturn([
+            'status' => 200,
+            'info'   => '成功'
+        ]);
+    }
+
+    //不通过
+    public function nopass() {
+        $id = I('post.id');
+        M('users')->where(['id' => $id])->save(['status' => 0]);
+        $this->ajaxReturn([
+            'status' => 200,
+            'info'   => '成功'
+        ]);
+    }
+
+    //修改信息
+    public function save() {
+        $id = I('post.id');
+        $phone = I('post.phone');
+        M('users')->where(['id' => $id])->save(['phone' => $phone]);
+        $this->ajaxReturn([
+            'status' => 200,
+            'info'   => '成功'
+        ]);
+    }
+
+    public function search() {
+        $input = I('post.');
+        switch($input['search_id']) {
+            case 1:
+                $map['nickname'] = ['like', '%'.$input['content'].'%'];
+                break;
+            case 2:
+                $map['phone'] = ['like', '%'.$input['content'].'%'];
+                break;
+            default:
+                $map['id'] = $input['content'];
+                break;
+        }
+        $map['role_id'] = 3;
+        $data = M('users')
+            ->where()
+            ->field('id as uid, nickname, gender, phone, status')
+            ->select();
+        $this->assign('data', $data);
+        $this->display('index');
+    }
 }
