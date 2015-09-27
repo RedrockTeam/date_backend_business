f7e87102a6d3d880cf56b6f36b7dc6fab0c75212
Lich2013 <longzy2013@gmail.com>
2015-09-27 16:08:19 +0800

    page

--- Application/Admin/Controller/ManageController.class.php
+++ Application/Admin/Controller/ManageController.class.php
@@ -59,11 +59,11 @@ class ManageController extends BaseController {
         $page = I('get.p');
         $data = $discover->where(['discover.status' => 2])
                              ->join('JOIN users ON discover.user_id = users.id')
-                             ->page($page, 1)
+                             ->page($page, 30)
                              ->field(['discover.id as discover_id', 'users.id as uid', 'users.nickname', 'discover.title', 'discover.caption', 'discover.content', 'discover.picture'])
                              ->select();
         $count      = $discover->where(['discover.status' => 2])->count();// 查询满足要求的总记录数
-        $Page       = new \Think\Page($count,1);// 实例化分页类 传入总记录数和每页显示的记录数
+        $Page       = new \Think\Page($count,30);// 实例化分页类 传入总记录数和每页显示的记录数
         $show       = $Page->show();// 分页显示输出
         $this->assign('page',$show);// 赋值分页输出
         $this->assign('data', $data);
